045eafebcc1f2138ec9f772c53e28c501b4143fc
allenmachary <allen@allen-laptop.(none)>
2010-03-16 16:17:23 +0300

    a template tag to return a tester with the number of samples he submitted.

--- apps/aquatest/templatetags/testers_tags.py
+++ apps/aquatest/templatetags/testers_tags.py
@@ -1,3 +1,4 @@
+import datetime
 from django import template
 
 from django.core.urlresolvers import reverse
@@ -13,6 +14,8 @@ import hq.utils as utils
 from datetime import timedelta
 import graphing.dbhelper as dbhelper
 from hq.models import *
+from reporters.models import Reporter
+from samples.models import Sample
 register = template.Library()
 
 from graphing.models import RawGraph
@@ -25,60 +28,29 @@ output_format = '%Y-%m-%d %H:%M'
 
 
 @register.simple_tag
-def get_tester_with_counts(user, startdate=None, enddate=None, use_blacklist=True):
+def get_counts_for_tester(reporter):
+    # Getting the count on the number of samples the user have submited
+    # this last month and the total count all time.
 
-    # todo:  query the global meta tables to get all the users
-    # and/or query the ExtUser table to get all the registered users.
-    totalspan = enddate-startdate
+    delta = timedelta(days=30)
+    enddate = datetime.today()
+    startdate = enddate - delta
     count_hash_month = {}
     count_hash_total = {}
-    extuser = ExtUser.objects.get(id=user.id)
     
-    # query the Ext user table to get all the reqisterd user with the same domain
-    # with the user.
-    #testers = ExtUser.objects.all().filter(domain=extuser.domain)
-    defs = FormDefModel.objects.all().filter(domain=extuser.domain)
     ret =""
-    #username_to_count_hash = { }
-    #if use_blacklist:
-        #domain_blacklist = extuser.domain.get_blacklist()
-    for fdef in defs:
-        try:
-            # don't do anything if we can't find a username column
-            username_col = fdef.get_username_column()
-            if not username_col:
-                logging.warning("No username column found in %s, will not display dashboard data." % fdef)
-                ret += '<p style="font-weight:bold; color:orange;">Warning: no username column found in %s, no dashboard data will be displayed for this form</p>' % fdef
-                continue
-            helper = fdef.db_helper
-            # let's get the usernames
-            usernames_to_filter = helper.get_uniques_for_column(username_col)
+       
+    count_month = Sample.objects.filter(taken_by=reporter, date_received__gte = startdate, date_received__lte = enddate ).count()
+    count_hash_month[reporter] = count_month
 
-            for user in usernames_to_filter:
-                count_month = user.objects.filter(date_received <= startdate).count()
-                count_hash_month[user] = count_month
-                
-                count_total = user.objects.all().count()
-                count_hash_total[user] = count_total
-        except Exception, e:
-            # this shouldn't blow up the entire view
-            logging.error("problem in dashboard display: %s" % e)
-            ret += '<p style="font-weight:bold; color:red;">problem in dashboard display.  Not all data will be visible.  Your error message is: %s</p>' % e
+    count_total = Sample.objects.filter(taken_by=reporter).count()
+    count_hash_total[reporter] = count_total
+
     
-    ret += '\n'
-    count = 1
-    for user in usernames_to_filter:
-        ret += '\n<tr class="%s">' % _get_class(count)
-        count += 1
-        ret += '<td>%s</td>' % (user.last_name)
-        ret += '<td>%s</td>' % (user.first_name)
-        ret += '<td>%s</td>' % (user.primary_phone)
-        ret += '<td>%s</td>' % (count_hash_month[user])
-        ret += '<td>%s</td>' % (count_hash_total[user])
-        ret += '<td><a href="">Edit</a> |<a href="">Delete</a></td>'
-        ret += '</tr>'
+    
+    ret += '<td>%s</td>' % (count_hash_month[reporter])
+    ret += '<td>%s</td>' % (count_hash_total[reporter])
 
-    ret += '</table>'
     return ret
 
 def _get_class(count):
