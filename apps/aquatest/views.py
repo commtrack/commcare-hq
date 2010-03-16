52fb7c4bc0872d486b874f26c2fe32548bea2bf8
allenmachary <allen@allen-laptop.(none)>
2010-03-16 16:19:10 +0300

    aquatest view of testers

--- apps/aquatest/views.py
+++ apps/aquatest/views.py
@@ -42,32 +42,290 @@ from hq.decorators import extuser_required
 
 from reporters.utils import *
 from reporters.views import message, check_reporter_form, update_reporter
-from reporters.models import Reporter, PersistantBackend, PersistantConnection
-
+#from reporters.models import Reporter, PersistantBackend, PersistantConnection
+from reporters.models import *
 
 logger_set = False
 
-@extuser_required()
-def testers(request):
-    template_name="testers.html"
-    context = {}
-    startdate, enddate = utils.get_dates(request)
-    context['startdate'] = startdate
-    context['enddate'] = enddate
-    context['view_name'] = 'hq.views.dashboard'
-    return render_to_response(request, template_name, context)
+
+from reporters.utils import *
+
+
+def message(req, msg, link=None):
+    return render_to_response(req,
+        "message.html", {
+            "message": msg,
+            "link": link
+    })
+
+def get_tester(current_user):
+    # todo: get the testers in the system with the same
+    # domain as the login user.
+    rep_profile = ReporterProfile.objects.filter(domain=current_user.domain)
+    reporters = []
+
+    if rep_profile:
+        for rep in rep_profile:
+            reporter = rep.reporter
+            reporters.append(reporter)
+    return reporters
 
 @extuser_required()
-def testers_delete(request):
-    template_name="testers.html"
-    return render_to_response(request, template_name, {})
+def index(req):
+    cur_user = ExtUser.objects.get(id=req.user.id)
+    reporters = get_tester(cur_user)
+    return render_to_response(req,
+        "testers/index.html", {
+        "reporters": paginated(req, reporters, prefix="rep"),
+        "groups":    paginated(req, ReporterGroup.objects.flatten(), prefix="grp"),
+    })
 
-@extuser_required()
-def testers_edit(request):
-    template_name="testers.html"
-    return render_to_response(request, template_name, {})
 
+def check_reporter_form(req):
+
+    # verify that all non-blank
+    # fields were provided
+    missing = [
+        field.verbose_name
+        for field in Reporter._meta.fields
+        if req.POST.get(field.name, "") == ""
+           and field.blank == False]
+
+    exists = []
+    alias = req.POST.get("alias","")
+    if Reporter.objects.filter( alias=alias ):
+        exists = ['alias']
+
+    # TODO: add other validation checks,
+    # or integrate proper django forms
+    return {
+        "missing": missing,
+        "exists": exists }
+
+@require_http_methods(["GET", "POST"])
 @extuser_required()
-def testers_add(request):
-    template_name="testers.html"
-    return render_to_response(request, template_name, {})
+def add_testers(req):
+    # NOTE/TODO:
+    # this is largely a copy paste job from rapidsms/apps/reporters/views.py
+    # method of the same name, and really doesn't belong here at all.
+
+    def get(req):
+        # pre-populate the "connections" field
+        # with a connection object to convert into a
+        # reporter, if provided in the query string
+        connections = []
+        if "connection" in req.GET:
+            connections.append(
+                get_object_or_404(
+                    PersistantConnection,
+                    pk=req.GET["connection"]))
+
+        cur_user = ExtUser.objects.get(id=req.user.id)
+        reporters = get_tester(cur_user)
+        return render_to_response(req,
+            "testers/testers.html", {
+
+                # display paginated reporters in the left panel
+                "reporters": paginated(req, reporters),
+
+                # pre-populate connections
+                "connections": connections,
+
+                # list all groups + backends in the edit form
+                "all_groups": ReporterGroup.objects.flatten(),
+                "all_backends": PersistantBackend.objects.all() })
+
+    @transaction.commit_manually
+    def post(req):
+        # check the form for errors
+        reporter_errors = check_reporter_form(req)
+        profile_errors = check_profile_form(req)
+
+        # if any fields were missing, abort.
+        missing = reporter_errors["missing"] + profile_errors["missing"]
+        exists = reporter_errors["exists"] + profile_errors["exists"]
+
+        if missing:
+            transaction.rollback()
+            return message(req,
+                "Missing Field(s): %s" % comma(missing),
+                link="/testers/add")
+        # if chw_id exists, abort.
+        if exists:
+            transaction.rollback()
+            return message(req,
+                "Field(s) already exist: %s" % comma(exists),
+                link="/testers/add")
+
+        try:
+            # create the reporter object from the form
+            rep = insert_via_querydict(Reporter, req.POST)
+            rep.save()
+
+            # add relevent connections
+            update_reporter(req, rep)
+            # create reporter profile
+            update_reporterprofile(req, rep, req.POST.get("chw_id", ""), \
+                                   req.POST.get("chw_username", ""))
+            # save the changes to the db
+            transaction.commit()
+
+            # full-page notification
+            return message(req,
+                "Testers %d added" % (rep.pk),
+                link="/testers")
+
+        except Exception, err:
+            transaction.rollback()
+            raise
+
+    # invoke the correct function...
+    # this should be abstracted away
+    if   req.method == "GET":  return get(req)
+    elif req.method == "POST": return post(req)
+
+
+
+@require_http_methods(["GET", "POST"])
+@extuser_required()
+def edit_testers(req, pk):
+    rep = get_object_or_404(Reporter, pk=pk)
+    rep_profile = get_object_or_404(ReporterProfile, reporter=rep)
+    rep.chw_id = rep_profile.chw_id
+    rep.chw_username = rep_profile.chw_username
+
+    def get(req):
+        return render_to_response(req,
+            "testers/testers.html", {
+
+                # display paginated reporters in the left panel
+                "reporters": paginated(req, Reporter.objects.all()),
+
+                # list all groups + backends in the edit form
+                "all_groups": ReporterGroup.objects.flatten(),
+                "all_backends": PersistantBackend.objects.all(),
+
+                # split objects linked to the editing reporter into
+                # their own vars, to avoid coding in the template
+                "connections": rep.connections.all(),
+                "groups":      rep.groups.all(),
+                "reporter":    rep })
+
+    @transaction.commit_manually
+    def post(req):
+
+        # if DELETE was clicked... delete
+        # the object, then and redirect
+        if req.POST.get("delete", ""):
+            pk = rep.pk
+            rep_profile.delete()
+            rep.delete()
+
+            transaction.commit()
+            return message(req,
+                "Tester %d deleted" % (pk),
+                link="/testers")
+
+        else:
+            # check the form for errors (just
+            # missing fields, for the time being)
+            reporter_errors = check_reporter_form(req)
+            profile_errors = check_profile_form(req)
+
+            # if any fields were missing, abort. this is
+            # the only server-side check we're doing, for
+            # now, since we're not using django forms here
+            missing = reporter_errors["missing"] + profile_errors["missing"]
+            if missing:
+                transaction.rollback()
+                return message(req,
+                    "Missing Field(s): %s" %
+                        ", ".join(missing),
+                    link="/testers/%s" % (rep.pk))
+
+            try:
+                # automagically update the fields of the
+                # reporter object, from the form
+                update_via_querydict(rep, req.POST).save()
+                # add relevent connections
+                update_reporter(req, rep)
+                # update reporter profile
+                update_reporterprofile(req, rep, req.POST.get("chw_id", ""), \
+                                       req.POST.get("chw_username", ""))
+
+                # no exceptions, so no problems
+                # commit everything to the db
+                transaction.commit()
+
+                # full-page notification
+                return message(req,
+                    "Tester %d updated" % (rep.pk),
+                    link="/testers")
+
+            except Exception, err:
+                transaction.rollback()
+                raise
+
+    # invoke the correct function...
+    # this should be abstracted away
+    if   req.method == "GET":  return get(req)
+    elif req.method == "POST": return post(req)
+
+@require_http_methods(["GET", "POST"])
+@extuser_required()
+def delete_testers(req, pk):
+    rep = get_object_or_404(Reporter, pk=pk)
+    rep_profile = get_object_or_404(ReporterProfile, reporter=rep)
+    rep_profile.delete()
+    rep.delete()
+
+    transaction.commit()
+    id = int(pk)
+    return message(req,
+        "Tester %d deleted" % (id),
+        link="/testers")
+
+
+def update_reporterprofile(req, rep, chw_id, chw_username):
+    try:
+        profile = ReporterProfile.objects.get(reporter=rep)
+    except ReporterProfile.DoesNotExist:
+        profile = ReporterProfile(reporter=rep, approved=True, active=True, \
+                                  guid = str(uuid.uuid1()).replace('-',''))
+        # reporters created through the webui automatically have the same
+        # domain and organization as the creator
+        extuser = req.extuser
+        profile.domain = extuser.domain
+        if extuser.organization == None:
+            profile.organization = Organization.objects.filter(domain=extuser.domain)[0]
+        else: profile.organization = extuser.organization
+    profile.chw_id = chw_id
+    profile.chw_username = chw_username
+    profile.save()
+
+def check_profile_form(req):
+    errors = {}
+    errors['missing'] = []
+    # we currently do not enforce the requirement for chw_id or chw_username
+    #if req.POST.get("chw_id", "") == "":
+    #    errors['missing'] = errors['missing'] + ["chw_id"]
+    #if req.POST.get("chw_username", "") == "":
+    #    errors['missing'] = errors['missing'] + ["chw_username"]
+
+    errors['exists'] = []
+    chw_id = req.POST.get("chw_id", "")
+    if chw_id:
+        # if chw_id is set, it must be unique for a given domain
+        rps = ReporterProfile.objects.filter(chw_id=req.POST.get("chw_id", ""), domain=req.extuser.domain)
+        if rps: errors['exists'] = ["chw_id"]
+    return errors
+
+def comma(string_or_list):
+    """ TODO - this could probably go in some sort of global util file """
+    if isinstance(string_or_list, basestring):
+        string = string_or_list
+        return string
+    else:
+        list = string_or_list
+        return ", ".join(list)
+
