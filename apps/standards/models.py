0303c9623351bca5b982d09a5579e1c1c2807063
allenmachary <allen@allen-laptop.(none)>
2010-03-16 16:22:59 +0300

    modified the modifed date field to make it optionial.

--- apps/standards/models.py
+++ apps/standards/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class WaterUseType(models.Model):
     description = models.CharField(max_length=100)
-    modified = models.DateTimeField()
+    modified = models.DateTimeField(blank=True, null=True)
     created = models.DateTimeField()
 
     def __unicode__(self):
@@ -13,7 +13,7 @@ class Standard(models.Model):
     name = models.CharField(max_length=50)
     govering_body = models.CharField(max_length=100)
     date_effective = models.DateField()
-    modified = models.DateTimeField()
+    modified = models.DateTimeField(blank=True, null=True)
     created = models.DateTimeField()
     water_use_type = models.ForeignKey(WaterUseType)
 
