dd39da386c1f062af42ef2c16dd8ed0ae4140b77
allenmachary <allen@allen-laptop.(none)>
2010-03-16 16:18:30 +0300

    urls to view add, edit and delete testers

--- apps/aquatest/urls.py
+++ apps/aquatest/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import *
 import aquatest.views as views
 
 urlpatterns = patterns('',
-    (r'^testers$', 'aquatest.views.testers'),
-    (r'^testers/add$', 'aquatest.views.testers_add'),
-    (r'^testers/(?P<pk>\d+)/edit$', 'aquatest.views.testers_edit'),
-    (r'^testers/(?P<pk>\d+)/delete$', 'aquatest.views.testers_delete'),
+    (r'^testers$', 'aquatest.views.index'),
+    (r'^testers/add$', 'aquatest.views.add_testers'),
+    (r'^testers/(?P<pk>\d+)$', 'aquatest.views.edit_testers'),
+    (r'^testers/(?P<pk>\d+)/delete$', 'aquatest.views.delete_testers'),
 )
