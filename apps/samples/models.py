dce599f365b42d85ff84bcff0406bddae7dbf582
allenmachary <allen@allen-laptop.(none)>
2010-03-18 12:06:25 +0300

    modified the parameter modified field to default now, samples not to be optional

--- apps/samples/models.py
+++ apps/samples/models.py
@@ -4,29 +4,16 @@ from datetime import datetime
 from standards.models import Standard, WaterUseType
 from reporters.models import Reporter
 from wqm.models import SamplingPoint
+from datetime import datetime
 
 # Create your Django models here, if you need them.
-class Sample(models.Model):
-    '''
-    This is sample
-    '''
-    taken_by = models.ForeignKey(Reporter)
-    sampling_point = models.ForeignKey(SamplingPoint)
-    notes = models.TextField()
-    date_taken = models.DateTimeField()
-    date_received = models.DateTimeField()
-    created = models.DateTimeField()
-    modified = models.DateTimeField(blank=True, null=True)
-
-    def __unicode__(self):
-        return self.notes
 
 class Parameter(models.Model):
     test_name = models.CharField(max_length=120)
-    unit = models.CharField(max_length=50)
+    unit = models.CharField(max_length=50, null=True, blank=True)
     lookup_hint = models.BooleanField()
     test_name_short = models.CharField(max_length=20)
-    modifed = models.DateTimeField(blank=True, null=True)
+    modifed = models.DateTimeField(blank=True, null=True, default=datetime.now())
 
     def __unicode__(self):
         return self.test_name
@@ -35,16 +22,33 @@ class MeasuredValue(models.Model):
     '''
     The measured values
     '''
-    sample = models.ForeignKey(Sample)
     parameter = models.ForeignKey(Parameter)
     # shuld be a charfield but for now it's a decimalfield.
-    # value = models.CharField(max_lenght=20)
-    value = models.DecimalField(max_digits=8, decimal_places=2)
+    value = models.CharField(max_length=20, help_text='the value measured')
+    #value = models.DecimalField(max_digits=8, decimal_places=2)
     modified = models.DateTimeField(blank=True, null=True)
-    created = models.DateTimeField()
+    created = models.DateTimeField(default=datetime.now())
 
     def __unicode__(self):
         return '%s' % (self.value)
+
+
+class Sample(models.Model):
+    '''
+    This is sample
+    '''
+    taken_by = models.ForeignKey(Reporter)
+    sampling_point = models.ForeignKey(SamplingPoint)
+    notes = models.TextField(null=True, blank=True)
+    measured_values = models.ForeignKey(MeasuredValue)
+    date_taken = models.DateTimeField()
+    date_received = models.DateTimeField()
+    created = models.DateTimeField()
+    modified = models.DateTimeField(blank=True, null=True)
+
+    def __unicode__(self):
+        return self.notes
+
 
 class ValueRule(models.Model):
     '''
@@ -55,7 +59,7 @@ class ValueRule(models.Model):
     standard = models.ForeignKey(Standard)
     water_use_type = models.ForeignKey(WaterUseType)
     modified = models.DateTimeField(blank=True, null=True)
-    created = models.DateTimeField()
+    created = models.DateTimeField(default=datetime.now() )
 
     def __unicode__(self):
         return self.description
@@ -69,7 +73,7 @@ class NormalRange(models.Model):
     maximum = models.IntegerField()
     minimum = models.IntegerField()
     modified = models.DateTimeField(blank=True, null=True)
-    created = models.DateTimeField()
+    created = models.DateTimeField(default=datetime.now())
 
     def __unicode__(self):
         return '%d - %d' % (self.minimum, self.maximum)
