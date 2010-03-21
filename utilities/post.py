b8c323dec0cc359c6fa94e132e60f053727e898e
Cory Zue <czue@dimagi.com>
2010-03-21 16:42:29 +0300

    reverting the post.py utility to do single form postings.

--- utilities/post.py
+++ utilities/post.py
@@ -23,17 +23,18 @@ curl_command = 'curl' #make sure curl is in your path
 
 # you can turn curl on and off here.  The alternative is standard python posts
 # but curl seems to be required for django to correctly handle multipart forms
-use_curl = True
+use_curl = False
 
 #filename = r'C:\Source\hq\commcare-hq\apps\backups\tests\data\backup.xml'
 #filename = r"C:\Documents and Settings\Cory Zue\Desktop\reg.xml"
 #filename = r'C:\Source\hq\commcare-hq\tests\deployment\multipart\multipart-3.txt'
 #filename = r'C:\Source\hq\commcare-hq\tests\deployment\multipart\multipart-1.post'
-filename = os.path.join(os.getcwd(), 'test-data', 'multipart-1.post')
+#filename = os.path.join(os.getcwd(), 'test-data', 'multipart-1.post')
+filename = r'C:\Users\czue\Downloads\aquatest-test.xml'
 
-domain_name = "BRAC"
-#content_type = "text/xml"
-content_type = "multipart/form-data; boundary=newdivider"
+domain_name = "itido"
+content_type = "text/xml"
+#content_type = "multipart/form-data; boundary=newdivider"
 
 string_url = 'http://%s/receiver/submit/%s' % (serverhost, domain_name)
 up = urlparse(string_url)
@@ -43,13 +44,14 @@ dict['User-Agent'] = 'CCHQ-submitfromfile-python-v0.1'
 try:
     file = open(filename, "rb")
     data = file.read()
+    print "data: %s" % data
     randint = random.random()
     # randomize some block in the file so it's not handled as a duplicate.
-    # data = data.replace("MTBZJTDO3SCT2ONXAQ88WM0CH", str(randint))
+    data = data.replace("LWRYWILK0ZCFO61Y7XIXGMDP5", str(randint))
     dict["content-type"] = content_type
     dict["content-length"] = len(data)
     if use_curl:
-        p = subprocess.Popen([curl_command,'--header','Content-type:multipart/mixed; boundary=newdivider', '--header', '"Content-length:%s' % len(data), 
+        p = subprocess.Popen([curl_command,'--header','Content-type:%s' % content_type, '--header', '"Content-length:%s' % len(data), 
                           '--data-binary', '@%s' % filename, '--request', 'POST', string_url],
                           stdout=PIPE,stderr=PIPE,shell=False)
         errors = p.stderr.read()
@@ -60,7 +62,7 @@ try:
         conn.request('POST', up.path, data, dict)
         resp = conn.getresponse()
         results = resp.read()
-        print "httplib gets ack\n%s" % results
+        print "httplib gets back\n%s" % results
 except Exception, e:
     print"problem submitting form: %s" % filename 
     print e
