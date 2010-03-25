c385001fc6a6dfef1dded007b0e08ba08541c125
Fred William <fred.haule@gmail.com>
2010-03-25 12:38:50 +0300

    adding search field and list filter

--- apps/smsnotifications/admin.py
+++ apps/smsnotifications/admin.py
@@ -4,6 +4,8 @@ from hq.models import *
 
 class SmsNotificationAdmin(admin.ModelAdmin):
     list_display = ('sampling_point', 'authorised_sampler', 'notification_type','digest','modified','created')
+    search_fields = ('sampling_point', 'authorised_sampler', 'notification_type','digest','modified','created')
+    list_filter = ['sampling_point', 'authorised_sampler', 'notification_type']
 admin.site.register(SmsNotification,SmsNotificationAdmin)
 
 
