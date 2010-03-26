10739016305ff54f125903b4968d49d253ea3f2a
allenmachary <allen@allen-laptop.(none)>
2010-03-26 16:46:41 +0300

    update on the admin site for samples.

--- apps/samples/admin.py
+++ apps/samples/admin.py
@@ -1,28 +1,39 @@
 from django.contrib import admin
 from samples.models import *
 
-'''
-'''
 class MeasuredValueInline(admin.TabularInline):
     model = MeasuredValue
     extra = 3
 
 class SampleAdmin(admin.ModelAdmin):
     list_display = ('taken_by','sampling_point','notes')
+    search_fields = ('taken_by','sampling_point','notes')
+    list_filter = ['taken_by','sampling_point']
     inlines = [MeasuredValueInline]
 admin.site.register(Sample, SampleAdmin)
 #admin.site.register(Sample)
 
-#class ParameterAdmin(admin.ModelAdmin):
-#    list_display = ('test_name','unit','lookup_hint','test_name_short')
-#    search_fields = ('test_name','unit','lookup_hint','test_name_short')
-#admin.site.register(Parameter, ParameterAdmin)
-admin.site.register(Parameter)
+class ParameterAdmin(admin.ModelAdmin):
+    list_display = ('test_name','unit','lookup_hint','test_name_short')
+    search_fields = ('test_name','unit','lookup_hint','test_name_short')
+    list_filter = ['test_name']
+admin.site.register(Parameter, ParameterAdmin)
 
-#admin.site.register(MeasuredValue)
-admin.site.register(ValueRule)
-admin.site.register(NormalRange)
+class ValueRuleAdmin(admin.ModelAdmin):
+    list_display = ('description','parameter','standard','water_use_type')
+    list_filter = ['standard']
+    search_fields = ('description','parameter','standard','water_use_type')
+admin.site.register(ValueRule,ValueRuleAdmin)
+
+class NormalRangeAdmin(admin.ModelAdmin):
+    list_display = ('description','value_rule','minimum','maximum')
+#    list_filter = []
+    search_fields = ('description','value_rule')
+admin.site.register(NormalRange,NormalRangeAdmin)
 
 class AbnormalRangeAdmin(admin.ModelAdmin):
-    fields = ['maximum', 'minimum', 'value_rule','description','color','remedialaction','created','modified']
-admin.site.register(AbnormalRange, AbnormalRangeAdmin)
+    list_display = ('description','value_rule','remedialaction','color','minimum','maximum')
+#    list_filter = ['']
+    search_fields = ('description','value_rule','remedialaction','color','minimum','maximum')
+admin.site.register(AbnormalRange,AbnormalRangeAdmin)
+
